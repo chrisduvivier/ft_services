b2715329aeb6771d827a8ac1753e46de18052030
Chris Duvivier <chrisduvivier15@gmail.com>
2021-02-12 12:06:05 +0100

    mysql alive as service: no interaction yet

--- setup.sh
+++ setup.sh
@@ -6,12 +6,18 @@ minikube start --vm-driver=docker
 
 eval $(minikube -p minikube docker-env)
 
-# build an image that runs nginx
+# build images from each Dockerfile
 docker build ./srcs/nginx -t nginx_img
+docker build ./srcs/mysql -t mysql_img
+docker build ./srcs/phpmyadmin -t phpmyadmin_img
+
+# metallb LoadBalancer config
+minikube addons enable metallb
+kubectl apply -f ./metallb-config.yaml
 
 # create a deployment and a service in minikube from the nginx image
 kubectl apply -f ./srcs/nginx/nginx-deployment.yaml
-# kubectl apply -f ./srcs/mysql/mysql-deployment.yaml
+kubectl apply -f ./srcs/mysql/mysql-deployment.yaml
 
 # have minikube expose the service locally
 minikube service nginx-service
---
+++ srcs/mysql/mysql_setup.sql
@@ -0,0 +1,15 @@
+
+-- create database for wordpress
+CREATE DATABASE wordpress_db;
+
+-- create user for wordpress
+GRANT ALL PRIVILEGES ON wordpress_db.* TO 'user'@'%' IDENTIFIED BY 'pass';
+
+-- create controluser for pma (required by pma) & grand privs in one breath 
+-- (the 'phpmyadmin' database itself will be created with the create_tables.sql script)
+GRANT ALL PRIVILEGES ON phpmyadmin.* TO 'user'@'%' IDENTIFIED BY 'pass';
+
+-- run script that will create the phpmyadmin database and populate it with the required tables
+\. create_tables.sql
+
+exit
